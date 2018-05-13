6eba3857e49f99ba599bf84b33667ba0eab0e39b
Acquil <34624268+Acquil@users.noreply.github.com>
2018-05-13 22:52:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 
 
-@app.route('/', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
     """This method handles the http requests for the Dialogflow webhook
     This is meant to be used in conjunction with the weather Dialogflow agent
