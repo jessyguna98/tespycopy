f2d059a4fc83341b4ec2ffb0df3c95a365d3b20b
Acquil <34624268+Acquil@users.noreply.github.com>
2018-05-14 08:54:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def webhook():
     """This method handles the http requests for the Dialogflow webhook
     This is meant to be used in conjunction with the weather Dialogflow agent
     """
-    req = request.get_json(silent=True, force=True)
-    '''try:
+    '''req = request.get_json(silent=True, force=True)
+    try:
         action = req.get('queryResult').get('action')
     except AttributeError:
         return 'json error'
@@ -42,7 +42,7 @@ def webhook():
     #return req
     
 
-def is_valid_doctor(req):
+'''def is_valid_doctor(req):
     """Returns a string containing text with a response to the user
     with the weather forecast or a prompt for more information
     Takes the city for the forecast and (optional) dates
@@ -50,7 +50,7 @@ def is_valid_doctor(req):
     """
     response = 'Bleah!'
     return response
-
+'''
 
 if __name__ == '__main__':
     app.run(debug=False, port=port, host='0.0.0.0')
