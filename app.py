614259a53b4782c2c68410d5d62eb16c0984bb1d
Acquil <34624268+Acquil@users.noreply.github.com>
2018-05-15 11:36:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
-
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-
 import json
-import os
-
-from flask import Flask
-from flask import request
-from flask import make_response
+from flask import Flask, request, make_response, jsonify
 
 # Flask app should start in global layout
 app = Flask(__name__)
+log = app.logger
 
 
-
-@app.route('/webhook', methods=['POST'])
+@app.route('/', methods=['POST'])
 def webhook():
     """This method handles the http requests for the Dialogflow webhook
     This is meant to be used in conjunction with the weather Dialogflow agent
@@ -28,36 +16,27 @@ def webhook():
         action = req.get('queryResult').get('action')
     except AttributeError:
         return 'json error'
-    '''
+    res = "Hurray!"
+    
     if action == 'isValidDoctor':
         res = is_valid_doctor(req)
-    else:
-        log.error('Unexpected action.')
+    #else:
+    #    log.error('Unexpected action.')
 
     print('Action: ' + action)
     print('Response: ' + res)
     
-    res='Gotcha!'
     return make_response(jsonify({'fulfillmentText': res}))
-    '''
-    #return req
-    res = {"fulfillmentText": "output"}
     
-        # If the request is not to the translate.text action throw an error
-    #log.error('Unexpected action requested: %s', json.dumps(req))
-    #res = {'speech': 'error', 'displayText': 'error'}
-
-    return make_response(jsonify(res))
-
-'''def is_valid_doctor(req):
+def is_valid_doctor(req):
     """Returns a string containing text with a response to the user
     with the weather forecast or a prompt for more information
     Takes the city for the forecast and (optional) dates
     uses the template responses found in weather_responses.py as templates
     """
-    response = 'Bleah!'
+    response = "Bleah!"
     return response
-'''
+
 
 if __name__ == '__main__':
     app.run(debug=False, port=port, host='0.0.0.0')
