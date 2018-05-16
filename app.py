1459c69789d133571ebfcab50c50b4d21da6e0b0
Acquil <34624268+Acquil@users.noreply.github.com>
2018-05-16 11:15:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def is_valid_doctor(req):
     
     doctor_name = req['queryResult']['parameters']['doctor_name']
     doctor_name = ''.join(doctor_name)
-    doctor_name = doctor_name.strip()
+    doctor_name = doctor_name.strip().title()
     conn = psycopg2.connect(database = "db0ntdu7buk51i", user = "tibwcqkplwckqf", password = "9cfed858b1d9206afb594c1c5cfacc5952b2fc21d440501daa3af5efd694313c", host = "ec2-107-20-249-68.compute-1.amazonaws.com", port = "5432")
     
     cur = conn.cursor()
