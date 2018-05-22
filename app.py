600c8f1906d7af14cf99dffc5ff1b39b4d78e25b
Acquil <acquil98@gmail.com>
2018-05-22 10:55:35 +0530

    Try 1 - changes 3

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def is_valid_doctor(req):
     # dept_cursor = dept_conn.cursor()
     response = "Found these results: \n"
 
-    cur.execute("SELECT doc_name, department_id from doc_list where doc_name LIKE '%"+ doctor_name+"%';")
+    cur.execute("SELECT doc_name from doc_list where doc_name LIKE '%"+ doctor_name+"%';")
     rows = cur.fetchall()
     # conn.close()
     conn2 = psycopg2.connect(database = "db0ntdu7buk51i", user = "tibwcqkplwckqf", password = "9cfed858b1d9206afb594c1c5cfacc5952b2fc21d440501daa3af5efd694313c", host = "ec2-107-20-249-68.compute-1.amazonaws.com", port = "5432")
