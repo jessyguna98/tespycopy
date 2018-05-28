e159b4a84f69ccc6a310050e57c361e1e3d18370
Acquil <acquil98@gmail.com>
2018-05-29 00:03:53 +0530

    complete display dept #3

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import json
 from flask import Flask, request, make_response, jsonify
 import psycopg2
-#from datetime import datetime
 
-# Flask app should start in global layout
 app = Flask(__name__)
 log = app.logger
 
@@ -21,7 +19,6 @@ def webhook():
         return 'json error'
 
     outputContexts = ""
-
 
     if action == 'isValidDoctor':
         res, outputContexts = is_valid_doctor(req)
@@ -87,17 +84,13 @@ def select_doctor(req):
 
     doctor_name = req['queryResult']['outputContexts'][0]['parameters']['doctor_name']
     doctor_name = ''.join(doctor_name)
-
-
     doctor_name = doctor_name.strip().title()
-
     doctor_name = doctor_name.replace("Dr. ","")
     doctor_name = doctor_name.replace("dr ","")
     doctor_name = doctor_name.replace("Dr ","")
 
     doctor_number = int(req['queryResult']['parameters']['number-integer'])
     # doctor_number = doctor_number - 1
-
 
     select_cur.execute("SELECT doc_name, doc_id from doc_list where doc_name LIKE '%"+ doctor_name+"%';")
     rows = select_cur.fetchall()
@@ -108,6 +101,7 @@ def select_doctor(req):
         if i  == doctor_number - 1:
             doctor_name = str(row[0])
             doctor_id = str(row[1])
+            break
         i += 1
 
 
@@ -153,18 +147,12 @@ def select_doctor_dept(req):
 
     i = 0
     for row in rows:
-
         if i  == doctor_number - 1:
             doctor_name = str(row[0])
             doctor_id = str(row[1])
+            break
         i += 1
 
-
-    # if doctor_number > 0 and doctor_number <= len (doctor_list):
-        # doctor_number = doctor_number - 1
-
-        # doctor_name = doctor_list[doctor_number]
-        # doc_id  = doctor_list[doctor_number]
 
     datetime = req['queryResult']['parameters']['date-time']['date_time']
     datetime = ''.join(datetime)
@@ -178,7 +166,7 @@ def select_doctor_dept(req):
         cur_insert_appointment.execute("INSERT INTO Appointments(Doctor_ID, App_Date, App_Time) values(' "+doctor_id+" ',' "+date_of_app+" ','"+time_of_app+"');")
         response = "Successfully booked with "+doctor_name+" on "+date_of_app+" at "+time_of_app
     else:
-        response = "I'm sorry. "+doctor_name+" seems to be busy at that time."
+        response = "I'm sorry. Dr. "+doctor_name+" seems to be busy at that time."
 
     conn.commit()
     conn.close()
@@ -194,27 +182,15 @@ def is_valid_doctor(req):
     date_of_app = datetime[:10]
     time_of_app = datetime[11:19]
 
-    # d1 = datetime.strptime(date1, '%Y-%m-%d')
-    # # day_string = d1.strftime('%Y-%m-%d')
-    #
-    # now = datetime.now()
-    # d2 = datetime.strptime(now, '%Y-%m-%d')
-
     doctor_name = req['queryResult']['parameters']['doctor_name']
     doctor_name = ''.join(doctor_name)
-
-
     doctor_name = doctor_name.strip().title()
-
     doctor_name = doctor_name.replace("Dr. ","")
     doctor_name = doctor_name.replace("dr ","")
     doctor_name = doctor_name.replace("Dr ","")
 
-
     conn = psycopg2.connect(database = "db0ntdu7buk51i", user = "tibwcqkplwckqf", password = "9cfed858b1d9206afb594c1c5cfacc5952b2fc21d440501daa3af5efd694313c", host = "ec2-107-20-249-68.compute-1.amazonaws.com", port = "5432")
-
     cur = conn.cursor()
-
     dept_cursor = conn.cursor()
 
     response = "Found these results: \nWhich Doctor where you looking for?\n"
@@ -227,14 +203,9 @@ def is_valid_doctor(req):
         doctor_id = str(row[2])
 
     response = "Invalid Choice!"
-    #conn.close()
-
-    # conn2 = psycopg2.connect(database = "db0ntdu7buk51i", user = "tibwcqkplwckqf", password = "9cfed858b1d9206afb594c1c5cfacc5952b2fc21d440501daa3af5efd694313c", host = "ec2-107-20-249-68.compute-1.amazonaws.com", port = "5432")
     cur2 = conn.cursor()
 
-
     if len(rows) ==1:
-
         cur.execute("SELECT * from Appointments where App_Date ='"+date_of_app+"' AND App_Time = '"+time_of_app+"' AND Doctor_ID='"+doctor_id+"' ")
         if_doc_busy_rows = cur.fetchall()
 
@@ -243,13 +214,6 @@ def is_valid_doctor(req):
             response = "Successfully booked with "+doctor_name+" on "+date_of_app+" at "+time_of_app
         else:
             response = "I'm sorry. "+doctor_name+" seems to be busy at that time."
-
-        # if d1>d2:
-        #     cur2.execute("INSERT INTO Appointments values('Qwerty','2018-05-30');")
-        #     # cur2.execute("INSERT INTO Appointments values(' "+doctor_name+" ',' "+date1+" ');")
-        #     response = "Successfully booked an appointment with Dr. " +doctor_name+ " on " +date1
-        # else:
-        #     response = "Invalid date to book an appointment"
 
     elif len(rows)>1:
         doctor_number=0                     #For the user to select from list of doctors
@@ -260,16 +224,13 @@ def is_valid_doctor(req):
             dept_list = dept_cursor.fetchall()
             response = response + str(doctor_number)  + ". Dr." + str(row[0]) + " of " + dept_list[0][0] + ",\n"
 
-
         response += "\n Please Choose by Number"
         outputContexts = "chooseDoctor"
-        # return make_response(jsonify({'fulfillmentText': response,'outputContexts':[{'name': "chooseDoctor"}]}))
 
     elif len(rows)==0:
         response = "Sorry! I couldn't find any doctor with that name."
 
     conn.commit()
-    # conn2.close()
     conn.close()
     return response,outputContexts
 
